b374717324e903b785c5e067406eeb6089f67921
rrprem <rheapremanand@gmail.com>
2020-03-11 14:17:50 -0400

    Lab 6a - connected mainactivity to question section - scrollview made - put first picture in

--- app/src/main/java/com/premanandrhea/lab06/MainActivity.java
+++ app/src/main/java/com/premanandrhea/lab06/MainActivity.java
@@ -2,13 +2,31 @@ package com.premanandrhea.lab06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.but);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStart();
+            }
+        });
     }
+
+    public void openStart(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
 }
