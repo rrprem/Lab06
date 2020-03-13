e732bae7cf6ae559750f7a695f96c094ea79c112
rrprem <rheapremanand@gmail.com>
2020-03-13 12:54:52 -0400

    Lab 6b - fixed photo errors - nested file for first question and added to main activity

--- app/src/main/java/com/premanandrhea/lab06/BtsActivity.java
+++ app/src/main/java/com/premanandrhea/lab06/BtsActivity.java
@@ -9,6 +9,6 @@ public class BtsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bts);
+        setContentView(R.layout.abts);
     }
 }
